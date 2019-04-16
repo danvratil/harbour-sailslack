8c3e7acc958c4de8d6380f28e2e1a15fffd7f857
Åke Engelbrektson <eson57@users.noreply.github.com>
2019-04-16 08:52:32 +0200

    Create harbour-sailslack-sv.ts
    Thanks for the app! Here's Swedish translation.

---
+++ translations/harbour-sailslack-sv.ts
@@ -0,0 +1,326 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="22"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="36"/>
+        <source>Unoffical Slack client for Sailfish OS</source>
+        <translation>Inofficiell Slack-klient för Sailfish OS</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="46"/>
+        <source>Browse channel and post new messages. Channels are updated real-time when new messages are posted.</source>
+        <translation>Kanalsurfa och posta nya meddelanden. Kanalerna uppdateras i realtid, när nya meddelanden postas.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="54"/>
+        <source>Source code and issues in &lt;a href=&apos;%1&apos;&gt;Github&lt;/a&gt;.</source>
+        <translation>Källkod och ärendehantering på &lt;a href=&apos;%1&apos;&gt;GitHub&lt;/a&gt;.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="62"/>
+        <source>Based on &lt;a href=&apos;%1&apos;&gt;Slackfish&lt;/a&gt; by Markus Sammallahti.</source>
+        <translation>Baserad på &lt;a href=&apos;%1&apos;&gt;Slackfish&lt;/a&gt; av Markus Sammallahti.</translation>
+    </message>
+</context>
+<context>
+    <name>Channel</name>
+    <message>
+        <location filename="../qml/pages/Channel.qml" line="43"/>
+        <source>Upload image</source>
+        <translation>Ladda upp bild</translation>
+    </message>
+</context>
+<context>
+    <name>ChannelList</name>
+    <message>
+        <location filename="../qml/pages/ChannelList.qml" line="23"/>
+        <source>Open chat</source>
+        <translation>Öppna chatt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ChannelList.qml" line="29"/>
+        <source>Join channel</source>
+        <translation>Gå med i kanalen</translation>
+    </message>
+</context>
+<context>
+    <name>ChannelListView</name>
+    <message>
+        <location filename="../qml/pages/ChannelListView.qml" line="72"/>
+        <source>Leave</source>
+        <translation>Lämna</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ChannelListView.qml" line="72"/>
+        <source>Close</source>
+        <translation>Stäng</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ChannelListView.qml" line="106"/>
+        <source>Unreads</source>
+        <translation>Olästa</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ChannelListView.qml" line="109"/>
+        <source>Channels</source>
+        <translation>Kanaler</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ChannelListView.qml" line="112"/>
+        <source>Direct messages</source>
+        <translation>Direktmeddelanden</translation>
+    </message>
+</context>
+<context>
+    <name>ChannelSelect</name>
+    <message>
+        <location filename="../qml/pages/ChannelSelect.qml" line="16"/>
+        <source>Join channel</source>
+        <translation>Gå med i kanalen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ChannelSelect.qml" line="21"/>
+        <source>No available channels</source>
+        <translation>Inga tillgängliga kanaler</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ChannelSelect.qml" line="42"/>
+        <source>Search</source>
+        <translation>Sök</translation>
+    </message>
+</context>
+<context>
+    <name>ChatSelect</name>
+    <message>
+        <location filename="../qml/pages/ChatSelect.qml" line="16"/>
+        <source>Open chat</source>
+        <translation>Öppna chatt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ChatSelect.qml" line="21"/>
+        <source>No available chats</source>
+        <translation>Inga tillgängliga chattar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ChatSelect.qml" line="42"/>
+        <source>Search</source>
+        <translation>Sök</translation>
+    </message>
+</context>
+<context>
+    <name>ConnectionPanel</name>
+    <message>
+        <location filename="../qml/pages/ConnectionPanel.qml" line="36"/>
+        <source>Reconnecting</source>
+        <translation>Återansluter</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ConnectionPanel.qml" line="44"/>
+        <source>Disconnected</source>
+        <translation>Frånkopplad</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ConnectionPanel.qml" line="51"/>
+        <source>Reconnect</source>
+        <translation>Återanslut</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ConnectionPanel.qml" line="44"/>
+        <source>No network connection</source>
+        <translation>Ingen nätverksanslutning</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message numerus="yes">
+        <location filename="../qml/cover/CoverPage.qml" line="61"/>
+        <source>Unread
+messages</source>
+        <comment>0</comment>
+        <translation>
+            <numerusform>Oläst
+meddelande</numerusform>
+            <numerusform>Olästa
+meddelanden</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>FileSend</name>
+    <message>
+        <location filename="../qml/pages/FileSend.qml" line="26"/>
+        <source>Upload image</source>
+        <translation>Skicka bild</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FileSend.qml" line="39"/>
+        <source>Select image</source>
+        <translation>Välj bild</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FileSend.qml" line="72"/>
+        <source>Send</source>
+        <translation>Skicka</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FileSend.qml" line="84"/>
+        <source>Uploading</source>
+        <translation>Laddar upp</translation>
+    </message>
+</context>
+<context>
+    <name>GroupLeaveDialog</name>
+    <message>
+        <location filename="../qml/pages/GroupLeaveDialog.qml" line="15"/>
+        <source>Leave %1</source>
+        <translation>Lämna %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/GroupLeaveDialog.qml" line="23"/>
+        <source>If you leave the private channel, you will no longer be able to see any of its messages. To rejoin the private channel, you will have to be re-invited.
+
+Are you sure you wish to leave?</source>
+        <translation>Om du lämnar den här privata kanalen, kommer du inte längre att kunna se några av dess meddelanden. För att åter gå med i den privata kanalen, måste du få en ny inbjudan.
+
+Vill du verkligen lämna?</translation>
+    </message>
+</context>
+<context>
+    <name>ImagePicker</name>
+    <message>
+        <location filename="../qml/dialogs/ImagePicker.qml" line="41"/>
+        <source>Select picture</source>
+        <translation>Välj bild</translation>
+    </message>
+</context>
+<context>
+    <name>Loader</name>
+    <message>
+        <location filename="../qml/pages/Loader.qml" line="52"/>
+        <source>Retry</source>
+        <translation>Försök igen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Loader.qml" line="95"/>
+        <source>Loading</source>
+        <translation>Läser in</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Loader.qml" line="109"/>
+        <source>Error loading team information</source>
+        <translation>Fel vid inläsning av laginformationen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Loader.qml" line="115"/>
+        <source>No network connection</source>
+        <translation>Ingen nätverksanslutning</translation>
+    </message>
+</context>
+<context>
+    <name>MessageListItem</name>
+    <message>
+        <location filename="../qml/pages/MessageListItem.qml" line="10"/>
+        <source>User Details</source>
+        <translation>Användarinformation</translation>
+    </message>
+</context>
+<context>
+    <name>MessageListView</name>
+    <message>
+        <location filename="../qml/pages/MessageListView.qml" line="38"/>
+        <source>User Details</source>
+        <translation>Användarinformation</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MessageListView.qml" line="100"/>
+        <source>Message %1%2</source>
+        <translation>Meddelande %1%2</translation>
+    </message>
+</context>
+<context>
+    <name>SlackClient</name>
+    <message>
+        <location filename="../src/slackclient.cpp" line="317"/>
+        <source>New message in %1</source>
+        <translation>Nytt meddelande i %1</translation>
+    </message>
+    <message>
+        <location filename="../src/slackclient.cpp" line="320"/>
+        <source>New message from %1</source>
+        <translation>Nytt meddelande från %1</translation>
+    </message>
+    <message>
+        <location filename="../src/slackclient.cpp" line="323"/>
+        <source>New message</source>
+        <translation>Nytt meddelande</translation>
+    </message>
+</context>
+<context>
+    <name>TeamList</name>
+    <message>
+        <location filename="../qml/pages/TeamList.qml" line="17"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TeamList.qml" line="24"/>
+        <source>Sign into a Workspace</source>
+        <translation>Logga in på en arbetsplats</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TeamList.qml" line="37"/>
+        <source>Sailslack</source>
+        <translation>Sailslack</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TeamList.qml" line="50"/>
+        <source>Logout from this Workspace</source>
+        <translation>Logga ut från denna arbetsplats</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TeamList.qml" line="51"/>
+        <source>Logging out</source>
+        <translation>Loggar ut</translation>
+    </message>
+</context>
+<context>
+    <name>UserView</name>
+    <message>
+        <location filename="../qml/pages/UserView.qml" line="15"/>
+        <source>Direct Messages</source>
+        <translation>Direktmeddelanden</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UserView.qml" line="61"/>
+        <source>Display Name</source>
+        <translation>Skärmnamn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UserView.qml" line="68"/>
+        <source>Email</source>
+        <translation>E-post</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UserView.qml" line="75"/>
+        <source>Timezone</source>
+        <translation>Tidszon</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UserView.qml" line="82"/>
+        <source>Phone</source>
+        <translation>Telefon</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UserView.qml" line="89"/>
+        <source>Skype</source>
+        <translation>Skype</translation>
+    </message>
+</context>
+</TS>
