317dd76573950de80109867c0025568b89c25676
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2021-05-07 19:51:41 +0800

    Create harbour-sailslack-zh_CN.ts

---
+++ translations/harbour-sailslack-zh_CN.ts
@@ -0,0 +1,409 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="22"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="36"/>
+        <source>Unoffical Slack client for Sailfish OS</source>
+        <translation>非官方旗鱼系统 Slack 客户端</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="46"/>
+        <source>Browse channel and post new messages. Channels are updated real-time when new messages are posted.</source>
+        <translation>浏览频道及粘贴新消息。当收到新消息时频道会实时更新。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="54"/>
+        <source>Source code and issues in &lt;a href=&apos;%1&apos;&gt;Github&lt;/a&gt;.</source>
+        <translation>源代码及问题位于 &lt;a href=&apos;%1&apos;&gt;Github&lt;/a&gt; 。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="62"/>
+        <source>Based on &lt;a href=&apos;%1&apos;&gt;Slackfish&lt;/a&gt; by Markus Sammallahti.</source>
+        <translation>基于，Markus Sammallahti 开发的  lt;a href=&apos;%1&apos;&gt;Slackfish&lt;/a&gt; 。</translation>
+    </message>
+</context>
+<context>
+    <name>Channel</name>
+    <message>
+        <location filename="../qml/pages/Channel.qml" line="43"/>
+        <source>Upload image</source>
+        <translation>更新图片</translation>
+    </message>
+</context>
+<context>
+    <name>ChannelList</name>
+    <message>
+        <location filename="../qml/pages/ChannelList.qml" line="23"/>
+        <source>Open chat</source>
+        <translation>开启对话</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ChannelList.qml" line="29"/>
+        <source>Join channel</source>
+        <translation>加入频道</translation>
+    </message>
+</context>
+<context>
+    <name>ChannelListView</name>
+    <message>
+        <location filename="../qml/pages/ChannelListView.qml" line="72"/>
+        <source>Leave</source>
+        <translation>离开</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ChannelListView.qml" line="72"/>
+        <source>Close</source>
+        <translation>关闭</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ChannelListView.qml" line="106"/>
+        <source>Unread</source>
+        <translation>未读</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ChannelListView.qml" line="108"/>
+        <source>Starred</source>
+        <translation>已星标</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ChannelListView.qml" line="111"/>
+        <source>Channels</source>
+        <translation>频道</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ChannelListView.qml" line="114"/>
+        <source>Direct messages</source>
+        <translation>私信</translation>
+    </message>
+</context>
+<context>
+    <name>ChannelSelect</name>
+    <message>
+        <location filename="../qml/pages/ChannelSelect.qml" line="16"/>
+        <source>Join channel</source>
+        <translation>加入频道</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ChannelSelect.qml" line="21"/>
+        <source>No available channels</source>
+        <translation>暂无可用频道</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ChannelSelect.qml" line="42"/>
+        <source>Search</source>
+        <translation>搜索</translation>
+    </message>
+</context>
+<context>
+    <name>ChatSelect</name>
+    <message>
+        <location filename="../qml/pages/ChatSelect.qml" line="16"/>
+        <source>Open chat</source>
+        <translation>开启对话</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ChatSelect.qml" line="21"/>
+        <source>No available chats</source>
+        <translation>没有可用对话</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ChatSelect.qml" line="42"/>
+        <source>Search</source>
+        <translation>搜索</translation>
+    </message>
+</context>
+<context>
+    <name>ConnectionPanel</name>
+    <message>
+        <location filename="../qml/pages/ConnectionPanel.qml" line="36"/>
+        <source>Reconnecting</source>
+        <translation>正在重新连接</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ConnectionPanel.qml" line="44"/>
+        <source>Disconnected</source>
+        <translation>已断开连接</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ConnectionPanel.qml" line="51"/>
+        <source>Reconnect</source>
+        <translation>重新连接</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ConnectionPanel.qml" line="44"/>
+        <source>No network connection</source>
+        <translation>暂无网络连接</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message numerus="yes">
+        <location filename="../qml/cover/CoverPage.qml" line="61"/>
+        <source>Unread
+messages</source>
+        <comment>0</comment>
+        <translation>
+            <numerusform>未读消息</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>FileSend</name>
+    <message>
+        <location filename="../qml/pages/FileSend.qml" line="26"/>
+        <source>Upload image</source>
+        <translation>更新图片</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FileSend.qml" line="39"/>
+        <source>Select image</source>
+        <translation>搜索图片</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FileSend.qml" line="72"/>
+        <source>Send</source>
+        <translation>发送</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FileSend.qml" line="84"/>
+        <source>Uploading</source>
+        <translation>正在上传</translation>
+    </message>
+</context>
+<context>
+    <name>GroupLeaveDialog</name>
+    <message>
+        <location filename="../qml/pages/GroupLeaveDialog.qml" line="15"/>
+        <source>Leave %1</source>
+        <translation>离开 %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/GroupLeaveDialog.qml" line="23"/>
+        <source>If you leave the private channel, you will no longer be able to see any of its messages. To rejoin the private channel, you will have to be re-invited.
+
+Are you sure you wish to leave?</source>
+        <translation>如果你离开此私人频道，你将不能看到任何消息。如果想要重新加入此私人频道，你必须重新被邀请。</translation>
+    </message>
+</context>
+<context>
+    <name>ImagePicker</name>
+    <message>
+        <location filename="../qml/dialogs/ImagePicker.qml" line="41"/>
+        <source>Select picture</source>
+        <translation>选择图片</translation>
+    </message>
+</context>
+<context>
+    <name>Loader</name>
+    <message>
+        <location filename="../qml/pages/Loader.qml" line="52"/>
+        <source>Retry</source>
+        <translation>重试</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Loader.qml" line="103"/>
+        <source>Loading</source>
+        <translation>正在加载</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Loader.qml" line="117"/>
+        <source>Error loading team information</source>
+        <translation>加载团队信息错误</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Loader.qml" line="123"/>
+        <source>No network connection</source>
+        <translation>暂无网络连接</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="38"/>
+        <source>Listening on port %1</source>
+        <translation>端口 %1 声音</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="38"/>
+        <source>Not listening on port %1</source>
+        <translation>端口 %1 没有声音</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="151"/>
+        <source>Advanced login options</source>
+        <translation>高级登录选项</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="88"/>
+        <source>You can login with your browser:</source>
+        <translation>你可以使用浏览器登录:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="92"/>
+        <source>Use browser</source>
+        <translation>使用浏览器</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="99"/>
+        <source>Or use the Sailfish WebView:</source>
+        <translation>或使用旗鱼 Webview:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="108"/>
+        <source>Or copy the link to use it on your desktop:</source>
+        <translation>或复制链接以在你的桌面使用:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="112"/>
+        <source>Copy link</source>
+        <translation>复制链接</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="122"/>
+        <source>Paste redirect url here</source>
+        <translation>在此粘贴重定向 url</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="128"/>
+        <source>Go</source>
+        <translation>前往</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="103"/>
+        <source>Use webview</source>
+        <translation>使用 webview</translation>
+    </message>
+</context>
+<context>
+    <name>MessageListItem</name>
+    <message>
+        <location filename="../qml/pages/MessageListItem.qml" line="23"/>
+        <source>Mark unread</source>
+        <translation>标记未读</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MessageListItem.qml" line="30"/>
+        <source>User Details</source>
+        <translation>用户详情</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MessageListItem.qml" line="80"/>
+        <source>Replies: %1</source>
+        <translation>回复:%1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MessageListItem.qml" line="17"/>
+        <source>Reply in thread</source>
+        <translation>在帖子回复</translation>
+    </message>
+</context>
+<context>
+    <name>MessageListView</name>
+    <message>
+        <location filename="../qml/pages/MessageListView.qml" line="64"/>
+        <source>User Details</source>
+        <translation>用户详情</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MessageListView.qml" line="110"/>
+        <source>Thread in #%1</source>
+        <translation>位于 #%1 帖子</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MessageListView.qml" line="150"/>
+        <source>Message %1%2</source>
+        <translation>消息 %1%2</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MessageListView.qml" line="150"/>
+        <source>Message thread in %1%2</source>
+        <translation>位于 %1%2 消息帖子</translation>
+    </message>
+</context>
+<context>
+    <name>SlackClient</name>
+    <message>
+        <location filename="../src/slackclient.cpp" line="365"/>
+        <source>in %1 @ %2</source>
+        <translation>位于 %1 @ %2</translation>
+    </message>
+    <message>
+        <location filename="../src/slackclient.cpp" line="368"/>
+        <source>from %1 @ %2</source>
+        <translation>来自 %1 @ %2</translation>
+    </message>
+    <message>
+        <location filename="../src/slackclient.cpp" line="371"/>
+        <source>New message</source>
+        <translation>新消息</translation>
+    </message>
+</context>
+<context>
+    <name>TeamList</name>
+    <message>
+        <location filename="../qml/pages/TeamList.qml" line="17"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TeamList.qml" line="24"/>
+        <source>Sign into a Workspace</source>
+        <translation>登录到工作空间</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TeamList.qml" line="37"/>
+        <source>Sailslack</source>
+        <translation>Sailslack</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TeamList.qml" line="50"/>
+        <source>Logout from this Workspace</source>
+        <translation>从此工作空间登出</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TeamList.qml" line="51"/>
+        <source>Logging out</source>
+        <translation>正在登出</translation>
+    </message>
+</context>
+<context>
+    <name>UserView</name>
+    <message>
+        <location filename="../qml/pages/UserView.qml" line="15"/>
+        <source>Direct Messages</source>
+        <translation>私信</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UserView.qml" line="70"/>
+        <source>Display Name</source>
+        <translation>显示名称</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UserView.qml" line="77"/>
+        <source>Email</source>
+        <translation>电子邮件</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UserView.qml" line="84"/>
+        <source>Timezone</source>
+        <translation>时区</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UserView.qml" line="91"/>
+        <source>Phone</source>
+        <translation>电话</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UserView.qml" line="98"/>
+        <source>Skype</source>
+        <translation>Skype</translation>
+    </message>
+</context>
+</TS>
